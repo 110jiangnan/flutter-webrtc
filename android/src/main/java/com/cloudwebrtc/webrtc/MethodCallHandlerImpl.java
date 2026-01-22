2f1b6ad2d0f14dbfbbbd8363e673bda0afbc2f13
Andres Pereira <andrexilol@gmail.com>
2026-01-22 03:02:31 +0000

    fix(android): Prevent ClassCastException validating track kind in method "mediaStreamAddTrack" (#1980)
    ## Description
    This issue causes a `ClassCastException` when trying to add an
    AudioTrack to the media stream. #651
    
    ```
    E  Failed to handle method call
      java.lang.ClassCastException: org.webrtc.AudioTrack cannot be cast to org.webrtc.VideoTrack
        at com.cloudwebrtc.webrtc.MethodCallHandlerImpl.onMethodCall(MethodCallHandlerImpl.java:617)
        at io.flutter.plugin.common.MethodChannel$IncomingMethodCallHandler.onMessage(MethodChannel.java:267)
        at io.flutter.embedding.engine.dart.DartMessenger.invokeHandler(DartMessenger.java:292)
        at io.flutter.embedding.engine.dart.DartMessenger.lambda$dispatchMessageToQueue$0$io-flutter-embedding-engine-dart-DartMessenger(DartMessenger.java:319)
        at io.flutter.embedding.engine.dart.DartMessenger$$ExternalSyntheticLambda0.run(D8$$SyntheticClass:0)
        at android.os.Handler.handleCallback(Handler.java:959)
        at android.os.Handler.dispatchMessage(Handler.java:100)
        at android.os.Looper.loopOnce(Looper.java:232)
        at android.os.Looper.loop(Looper.java:317)
        at android.app.ActivityThread.main(ActivityThread.java:8705)
        at java.lang.reflect.Method.invoke(Native Method)
        at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:580)
        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:886)
    ```
    
    ## Changes
    - Validate the track kind before try to use an AudioTrack as VideoTrack
    
    Co-authored-by: CloudWebRTC <duanweiwei1982@gmail.com>
    (cherry picked from commit ab2f2e08f685078b8eb6de000ec12ec5c70c7bba)

--- android/src/main/java/com/cloudwebrtc/webrtc/MethodCallHandlerImpl.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/MethodCallHandlerImpl.java
@@ -634,7 +634,7 @@ public class MethodCallHandlerImpl implements MethodCallHandler, StateProvider {
           if (renderer.checkMediaStream(streamId, "local")) {
             synchronized (localTracks) {
               LocalTrack track = localTracks.get(trackId);
-              if(track != null) {
+              if(track != null && track.kind().equals("video")) {
                 renderer.setVideoTrack((VideoTrack) track.track);
               }
             }
